3b425b74b46fc54d30bb21abc404f0ca2caf0b06
raphael-caninde <raphael.caninde@gmail.com>
2021-11-18 16:17:55 -0300

    req 3

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Search from '../pages/Search';
+import Cart from '../pages/Cart';
 
 export default class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Search } />
+        <Route exact path="/cart" component={ Cart } />
       </Switch>
     );
   }
